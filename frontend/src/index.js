893cdc3cfdb0f55ffad69a6e53740167ed1bd9b1
vedran-brnjetic <Vedran.Brnjetic@metropolia.fi>
2018-02-04 13:23:03 +0200

    Cleanup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import HeaderMenu from './components/header_menu';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
