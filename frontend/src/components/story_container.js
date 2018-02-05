96596cea8e7ecd9ebf878f2908508e8049036695
Vedran Brnjetic <vedranb@metropolia.fi>
2018-02-05 22:55:51 +0200

    Fixed the error in an illogical way, need too check it out

--- frontend/src/components/story_container.js
+++ frontend/src/components/story_container.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
-import { 
-	Card, 
-	Button, 
-	CardImg, 
-	CardTitle, 
-	CardText, 
-	CardDeck, 
-	CardBody 
+import {
+	Card,
+	Button,
+	CardImg,
+	CardTitle,
+	CardText,
+	CardDeck,
+	CardBody
 } from 'reactstrap';
 
-class StoryContainer extends Component 
+class StoryContainer extends Component
 {
 	constructor(props){
 		super(props);
@@ -17,9 +17,9 @@ class StoryContainer extends Component
       	error: null,
       	isLoaded: false,
       	stories: []
-    	};	
+    	};
 	}
-	
+
 	componentWillMount() {
 		fetch("http://api.byelingual.me:8000/stories/")
 			.then( res => res.json())
@@ -28,49 +28,46 @@ class StoryContainer extends Component
 					this.setState({
 						isLoaded: true,
 						stories: result.stories
-					});				
+					});
 				},
 				(error) => {
 					this.setState({
 						isLoaded: true,
-						error	 
-					});				
-				}			
-			)	
+						error
+					});
+				}
+			)
 	}
-   
+
    render () {
    	let {error, isLoadad, stories } = this.state;
    	if (error) {
-			return <div>Error: {error.message}</div>;
-   	} 
+			return <div>Error: { error.message }</div>;
+   	}
    	else if(!isLoadad){
-			return <div>Loading...</div>;
-   	} 
-   	else {
 			return (
-		    	<CardDeck>
+		    	<CardDeck className="col-md-12">
 		    		{
 		    			stories.map(
 		    				story => (
       						<Card key={story.name}>
-        							<CardImg top width="100%" src={ "http://byelingual.me:8000/media/" + story.image } alt={ story.description } />
-        							<CardBody>
-          							<CardTitle>{ story.name }</CardTitle>
-          							<CardText>{ story.description }</CardText>
-          							<Button>Buy Now!</Button>
+        						<CardImg top width="100%" src={ "http://byelingual.me:8000/media/" + story.image } alt={ story.description } />
+        						<CardBody>
+          						<CardTitle>{ story.name }</CardTitle>
+          						<CardText>{ story.description }</CardText>
+          						<Button disabled>Buy Now!</Button>
        							</CardBody>
       						</Card>
       					)
       				)
    				}
     			</CardDeck>
-			);   
+			);
    	}
-   
-   
-   
-  
+
+
+
+
 	};
 }
 export default StoryContainer;
