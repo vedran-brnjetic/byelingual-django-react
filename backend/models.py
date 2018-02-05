e73d36003f1ca612855dbac5de8b446143cc9b0a
Vedran Brnjetic <vedran.brnjetic@metropolia.fi>
2018-02-05 21:20:10 +0000

    changed admin object string representation for stories

--- backend/models.py
+++ backend/models.py
@@ -5,4 +5,7 @@ class Story(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='story_images',blank=True)
+
+    def __str__(self):
+        return self.name
     
