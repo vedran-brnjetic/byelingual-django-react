97862773310793687e36b60338649ea09f19d016
Vedran Brnjetic <vedranb@metropolia.fi>
2018-02-07 10:46:10 +0200

    Added Player profile

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Story(models.Model):
@@ -8,6 +10,21 @@ class Story(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     class Meta:
 	verbose_name_plural = "stories"
+
+class Player(moodels.Model):
+    user = models.OneToOneField( User.id, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='player_profiles', default='NoProfileImage.jpg')
+    public_name = models.CharField(max_length=30, default='user_public_name')
+    language = models.CharField(max_length=3, default='en') #store just the country code and interpolate from that
+
+@receiver(post_save, sender=User)
+def create_player_profile(sender, instance, created, **kwargs):
+    if created:
+        Player.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_player_profile(sender, instance, **kwargs):
+    instance.player.save
