7e86fa54f1132b557414e3203c112753931cdadc
Vedran Brnjetic <vedranb@metropolia.fi>
2018-02-07 10:48:14 +0200

    Updated admin and api views for player profile

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Story
+from .models import Story, Player
 # Register your models here.
 
 admin.site.register(Story)
+admin.site.register(Player)
