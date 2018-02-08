348b6e6c8e8b7264be16bad6ca6453aa31c1a921
Vedran Brnjetic <vedran.brnjetic@metropolia.fi>
2018-02-08 08:17:19 +0000

    fixed some syntax

--- backend/models.py
+++ backend/models.py
@@ -21,8 +21,8 @@ class Player(models.Model):
     avatar = models.ImageField(upload_to='player_profiles', default='NoProfileImage.jpg')
     public_name = models.CharField(max_length=30, default='user_public_name')
     language = models.CharField(max_length=3, default='en') #store just the country code and interpolate from that
-    isAuthor = models.BooleanField(default=false)
-    authorBiography = models.TextField(blank=true)
+    isAuthor = models.BooleanField(default=False)
+    authorBiography = models.TextField(blank=True)
 
 @receiver(post_save, sender=User)
 def create_player_profile(sender, instance, created, **kwargs):
